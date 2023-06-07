b4f3985ea4a49d86796cbf42b143096e12ac7781
KubiBatuOner <kbatuhanoner@yahoo.com>
2023-06-07 14:17:06 +0300

    Şehirlere renk eklendi

--- deprem-proje/src/components/SehirPages/OsmaniyePage.jsx
+++ deprem-proje/src/components/SehirPages/OsmaniyePage.jsx
@@ -1,8 +1,43 @@
-import Map from "../Map";
+import { ReactComponent as Map } from "../../images/svgOsmaniye.svg";
 import Osmaniye from "../Sehirler/Osmaniye";
 import NewHeader from "../newHeader";
+import { useEffect } from "react";
 
 export default function OsmaniyePage() {
+  useEffect(() => {
+    let label1 = document.querySelector("#sanliurfa");
+    let label2 = document.querySelector("#gaziantep");
+    let label3 = document.querySelector("#kilis");
+    let label4 = document.querySelector("#osmaniye");
+    let label5 = document.querySelector("#adana");
+    let label6 = document.querySelector("#kahramanmaras");
+    let label7 = document.querySelector("#adiyaman");
+    let label8 = document.querySelector("#malatya");
+    let label9 = document.querySelector("#hatay");
+    let label10 = document.querySelector("#diyarbakir");
+    addLabelText(label1, "Şanlıurfa");
+    addLabelText(label2, "Gaziantep");
+    addLabelText(label3, "Kilis");
+    addLabelText(label4, "Osmaniye");
+    addLabelText(label5, "Adana");
+    addLabelText(label6, "Kahramanmaraş");
+    addLabelText(label7, "Adıyaman");
+    addLabelText(label8, "Malatya");
+    addLabelText(label9, "Hatay");
+    addLabelText(label10, "Diyarbakır");
+    function addLabelText(bgPath, labelText) {
+      let bbox = bgPath.getBBox();
+      let x = bbox.x + bbox.width / 2.2;
+      let y = bbox.y + bbox.height / 1.7;
+      let textElem = document.createElementNS(bgPath.namespaceURI, "text");
+      textElem.setAttribute("x", x);
+      textElem.setAttribute("y", y);
+      textElem.setAttribute("text-anchor", "middle");
+      textElem.classList.add("label-text");
+      textElem.textContent = labelText;
+      bgPath.after(textElem);
+    }
+  }, []);
   return (
     <div className="flex justify-center items-center h-auto rounded-[40px] bg-[#D9E8E7]">
       <div className="flex bg-white w-full rounded-[40px] my-5 mx-5 ">
